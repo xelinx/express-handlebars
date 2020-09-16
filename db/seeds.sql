fbc67fc4058ba85fd9ce1978323f9a68e6900a06
Fog <lin.emay@gmail.com>
2020-09-15 23:37:40 -0700

    update seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name, devoured) VALUES('Deluxe', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Special', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Hamburger', false);
