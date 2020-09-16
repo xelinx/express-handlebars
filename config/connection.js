214f1e1670a02e41fc186a3381f04faf59ffc69a
Fog <lin.emay@gmail.com>
2020-09-15 23:10:54 -0700

    fix error

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
   connection = mysqul.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL){
     password: "",
     database: "cat_db"
   });
-});
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
