22672e8ce129e7d2c4e20ce927672c637a36f80e
Fog <lin.emay@gmail.com>
2020-09-16 23:13:00 -0700

    update model

--- models/burgers.js
+++ models/burgers.js
@@ -1,7 +1,7 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var cat = {
+var burger = {
   all: function(cb) {
     orm.all("burgers", function(res) {
       cb(res);
@@ -15,6 +15,11 @@ var cat = {
   },
   update: function(objColVals, condition, cb) {
     orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
       cb(res);
     });
   }
