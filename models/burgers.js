aca2e25f89c69d22bbd896051edeece8231f09af
Fog <lin.emay@gmail.com>
2020-09-17 01:22:00 -0700

    update model

--- models/burgers.js
+++ models/burgers.js
@@ -2,24 +2,24 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  insertOne: function (val, cb) {
+    orm.insertOne("burgers", "burger_name", val, function (res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+  updateOne: function (boolean, condition, cb) {
+    orm.updateOne("burgers", "devoured", boolean, condition, function (res) {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+  deleteOne: function (condition, cb) {
+    orm.deleteOne("burgers", condition, function (res) {
       cb(res);
     });
   }
