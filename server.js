bcfd70fbf6972d4b44180d35ead3ffbba2530d0a
Fog <lin.emay@gmail.com>
2020-09-17 00:30:35 -0700

    update server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname, "/public"));
+app.use(express.static(__dirname + "/public"));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
